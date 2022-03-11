9473753bcfa5f6c531af254415d72e4614e42de0
Anderson <99144364+AndersonMGarcia@users.noreply.github.com>
2022-03-11 19:49:25 -0300

    Classes e Objetos

---
+++ 01-CriandoClasses_e_Objetos/Caneta.php
@@ -0,0 +1,30 @@
+<?php
+
+    class caneta{
+        var $modelo;
+        var $cor;
+        var $ponta;
+        var $carga;
+        var $tampada;
+
+        function rabiscar(){
+
+            if ($this->tampada){
+                echo "<p>Erro! Não posso rabiscar, a caneta esta tampada!</p>";
+            }
+            else{
+                echo "<p>Estou rabiscando! </p>";
+            }
+            
+        }
+
+        function tampar(){
+            $this->tampada = true;
+        }
+
+        function destampar(){
+            $this->tampada = false;
+        }
+
+        
+    }
